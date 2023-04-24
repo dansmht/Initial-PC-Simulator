8fe593bbf02bb3b13495b86f6c43f0b7943d3ee7
Daniil Smakhtsin <dansmht16@gmail.com>
2023-04-24 18:43:29 +0300

    disable native context menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import { DesktopPage } from "./pages";
 
 export const App = () => {
+
+  useEffect(() => {
+    const disableNativeContextMenu = (event: MouseEvent) => event.preventDefault();
+
+    document.addEventListener("contextmenu", disableNativeContextMenu);
+
+    return () => {
+      document.removeEventListener("contextmenu", disableNativeContextMenu);
+    };
+  }, []);
+
   return (
     <DesktopPage/>
   );
