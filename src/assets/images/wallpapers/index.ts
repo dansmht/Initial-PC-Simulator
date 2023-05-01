2aec20b4a2acf1f0a2886aeffc4b0802e807b4c8
Daniil Smakhtsin <dansmht16@gmail.com>
2023-05-01 15:56:10 +0300

    add wallpapers

--- src/assets/images/wallpapers/index.ts
+++ src/assets/images/wallpapers/index.ts
@@ -1,4 +1,15 @@
 import wallpaper1Src from "../../../assets/images/wallpapers/wallpaper-1.jpg";
 import wallpaper2Src from "../../../assets/images/wallpapers/wallpaper-2.jpg";
+import wallpaper3Src from "../../../assets/images/wallpapers/wallpaper-3.jpg";
+import wallpaper4Src from "../../../assets/images/wallpapers/wallpaper-4.jpg";
+import wallpaper5Src from "../../../assets/images/wallpapers/wallpaper-5.jpg";
+import wallpaper6Src from "../../../assets/images/wallpapers/wallpaper-6.jpg";
 
-export const wallpapers = [wallpaper1Src, wallpaper2Src];
+export const wallpapers = [
+  wallpaper1Src,
+  wallpaper2Src,
+  wallpaper3Src,
+  wallpaper4Src,
+  wallpaper5Src,
+  wallpaper6Src,
+];
