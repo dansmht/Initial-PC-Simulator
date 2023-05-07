ca9735e04a4cee9f04f44006f63b189b80b0f758
Daniil Smakhtsin <dansmht16@gmail.com>
2023-05-07 07:50:37 +0300

    remove button & fix style

--- src/modules/Desktop/components/DesktopWallpaper/DesktopWallpaper.tsx
+++ src/modules/Desktop/components/DesktopWallpaper/DesktopWallpaper.tsx
@@ -8,14 +8,9 @@ type Props = HasChildren;
 
 export const DesktopWallpaper: FC<Props> = ({children}) => {
   const activeWallpaper = useWallpaperStore((state) => state.activeWallpaper);
-  const nextWallpaper = useWallpaperStore((state) => state.setActiveWallpaper);
 
   return (
     <div className={styles.DesktopWallpaper} style={{backgroundImage: `url(${wallpapers[activeWallpaper]})`}}>
-      DesktopWallpaper
-      <button onClick={() => nextWallpaper(Math.random() < 0.5 ? 0 : 1)}>
-        NEXT
-      </button>
       {children}
     </div>
   );
